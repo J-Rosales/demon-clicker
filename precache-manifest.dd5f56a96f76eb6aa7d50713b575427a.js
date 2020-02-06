ed679abdaa1ae7138a8cbdfef407483ead458430
Jose Rosales <jarc_lex@hotmail.com>
2020-02-06 14:34:10 -0600

    Updates

---
+++ precache-manifest.dd5f56a96f76eb6aa7d50713b575427a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59da9341e33f31b9183aa54bc1ed90c4",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "63b80a251924bff451a3",
+    "url": "/deploy-test/static/css/main.adf1ba46.chunk.css"
+  },
+  {
+    "revision": "4a06616c50b24371deec",
+    "url": "/deploy-test/static/js/2.94fc77b6.chunk.js"
+  },
+  {
+    "revision": "c1b8259ff9f49c3d0530649bca7fb18b",
+    "url": "/deploy-test/static/js/2.94fc77b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63b80a251924bff451a3",
+    "url": "/deploy-test/static/js/main.650e08d6.chunk.js"
+  },
+  {
+    "revision": "e539c7fb54ee067c5531",
+    "url": "/deploy-test/static/js/runtime-main.f1fe7fca.js"
+  },
+  {
+    "revision": "b3e2b65435da838fb7281f73a901c6e5",
+    "url": "/deploy-test/static/media/bg_castle.b3e2b654.jfif"
+  }
+]);
